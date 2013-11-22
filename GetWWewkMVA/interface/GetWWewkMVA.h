ccf7cee4ac53dd567edd78c087c67621f3fb98d9
Andrea <massironi.andrea@gmail.com>
2013-11-22 11:20:00 +0100

    up

---
+++ GetWWewkMVA/interface/GetWWewkMVA.h
@@ -0,0 +1,38 @@
+#ifndef GetWWewkMVA_h
+#define GetWWewkMVA_h
+
+#include "TMVA/Reader.h"
+
+class GetWWewkMVA {
+
+ public:
+  GetWWewkMVA();
+  ~GetWWewkMVA();
+
+  void init(std::string methodName, std::string weightsfile);
+  double getValue(double jetpt1,double jetpt2,double detajj,double logmjj,double pt1,double pt2,double ptll,double zeppl1,double zeppl2,double zeppll,double dphilljetjet);
+
+ private:
+
+  TMVA::Reader* theReader;
+
+  //common variables
+  float jetpt1_;
+  float jetpt2_;
+  float detajj_;
+  float logmjj_;
+  float pt1_;
+  float pt2_;
+  float ptll_;
+  float zeppl1_;
+  float zeppl2_;
+  float zeppll_;
+  float dphilljetjet_;
+
+  std::string methodname_;
+
+  bool isInit;
+
+};
+
+#endif
---
+++ GetWWewkMVA/src/GetWWewkMVA.cc
@@ -0,0 +1,73 @@
+#include "WWewkMvaInCMSSW/GetWWewkMVA/interface/GetWWewkMVA.h"
+
+GetWWewkMVA::GetWWewkMVA():isInit(false){
+ init(
+   "BDT",
+ getenv("CMSSW_BASE")+std::string("/src/WWewkMvaInCMSSW/GetWWewkMVA/data/TMVA_2j_BDT.weights.xml")
+     );
+}
+
+GetWWewkMVA::~GetWWewkMVA(){
+ delete theReader;
+}
+
+void GetWWewkMVA::init(std::string methodName, std::string weightsfile){
+
+ if (isInit) return;
+
+ theReader = new TMVA::Reader();
+
+ theReader->AddVariable("jetpt1",       &jetpt1_]);
+ theReader->AddVariable("jetpt2",       &jetpt2_);
+ theReader->AddVariable("detajj",       &detajj_);
+ theReader->AddVariable("log(mjj)",     &logmjj_);
+ theReader->AddVariable("pt1",          &pt1_);
+ theReader->AddVariable("pt2",          &pt2_);
+ theReader->AddVariable("ptll",         &ptll_);
+
+ theReader->AddVariable("abs(eta1-(jeteta1+jeteta2)/2)/detajj", &zeppl1_);
+ theReader->AddVariable("abs(eta2-(jeteta1+jeteta2)/2)/detajj", &zeppl2_);
+ theReader->AddVariable("abs(yll-(jeteta1+jeteta2)/2)/detajj",  &zeppll_);
+ theReader->AddVariable("dphilljetjet",    &dphilljetjet_);
+
+ theReader->BookMVA(methodName,weightsfile);
+
+ methodname_ = methodName;
+ isInit = true;
+}
+
+
+double GetWWewkMVA::getValue(
+   double jetpt1,
+   double jetpt2,
+   double detajj,
+   double logmjj,
+   double pt1,
+   double pt2,
+   double ptll,
+   double zeppl1,
+   double zeppl2,
+   double zeppll,
+   double dphilljetjet
+                          ){
+
+    if (!isInit) {
+     std::cerr << "MVA reader not initialized!" << std::endl;
+     return -999.;
+    }
+
+    jetpt1_ = jetpt1_;
+    jetpt2_ = jetpt2_;
+    detajj = detajj_;
+    logmjj = logmjj_;
+    pt1 = pt1_;
+    pt2 = pt2_;
+    ptll = ptll_;
+    zeppl1 = zeppl1_;
+    zeppl2 = zeppl2_;
+    zeppll = zepll_;
+    dphilljetjet = dphilljetjet_; 
+
+    return theReader->EvaluateMVA( methodname_ );
+
+                          }
