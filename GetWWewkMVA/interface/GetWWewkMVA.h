1953d7799988d076ead3695607e3ed3647b5e30a
Andrea <massironi.andrea@gmail.com>
2013-12-30 15:47:08 +0100

    new training

--- GetWWewkMVA/interface/GetWWewkMVA.h
+++ GetWWewkMVA/interface/GetWWewkMVA.h
@@ -10,7 +10,8 @@ class GetWWewkMVA {
   ~GetWWewkMVA();
 
   void init(std::string methodName, std::string weightsfile);
-  double getValue(double jetpt1,double jetpt2,double detajj,double logmjj,double pt1,double pt2,double ptll,double zeppl1,double zeppl2,double zeppll,double dphilljetjet);
+//   double getValue(double jetpt1,double jetpt2,double detajj,double logmjj,double pt1,double pt2,double ptll,double zeppl1,double zeppl2,double zeppll,double dphilljetjet);
+  double getValue(double jetpt1,double jetpt2,double detajj,double logmjj,double zeppl1,double zeppl2,double zeppll);
 
  private:
 
@@ -21,13 +22,13 @@ class GetWWewkMVA {
   float jetpt2_;
   float detajj_;
   float logmjj_;
-  float pt1_;
-  float pt2_;
-  float ptll_;
+//   float pt1_;
+//   float pt2_;
+//   float ptll_;
   float zeppl1_;
   float zeppl2_;
   float zeppll_;
-  float dphilljetjet_;
+//   float dphilljetjet_;
 
   std::string methodname_;
 
--- GetWWewkMVA/src/GetWWewkMVA.cc
+++ GetWWewkMVA/src/GetWWewkMVA.cc
@@ -2,9 +2,13 @@
 
 GetWWewkMVA::GetWWewkMVA():isInit(false){
  init(
-   "BDTG",
- getenv("CMSSW_BASE")+std::string("/src/WWewkMvaInCMSSW/GetWWewkMVA/data/TMVA_WWewk_BDTG.weights.xml")
+   "MLP",
+ getenv("CMSSW_BASE")+std::string("/src/WWewkMvaInCMSSW/GetWWewkMVA/data/TMVA_WWewk_MLP.weights.xml")
      );
+//  init(
+//    "BDTG",
+//  getenv("CMSSW_BASE")+std::string("/src/WWewkMvaInCMSSW/GetWWewkMVA/data/TMVA_WWewk_BDTG.weights.xml")
+//      );
 }
 
 GetWWewkMVA::~GetWWewkMVA(){
@@ -21,14 +25,14 @@ void GetWWewkMVA::init(std::string methodName, std::string weightsfile){
  theReader->AddVariable("jetpt2",       &jetpt2_);
  theReader->AddVariable("detajj",       &detajj_);
  theReader->AddVariable("log(mjj)",     &logmjj_);
- theReader->AddVariable("pt1",          &pt1_);
- theReader->AddVariable("pt2",          &pt2_);
- theReader->AddVariable("ptll",         &ptll_);
+//  theReader->AddVariable("pt1",          &pt1_);
+//  theReader->AddVariable("pt2",          &pt2_);
+//  theReader->AddVariable("ptll",         &ptll_);
 
  theReader->AddVariable("abs(eta1-(jeteta1+jeteta2)/2)/detajj", &zeppl1_);
  theReader->AddVariable("abs(eta2-(jeteta1+jeteta2)/2)/detajj", &zeppl2_);
  theReader->AddVariable("abs(yll-(jeteta1+jeteta2)/2)/detajj",  &zeppll_);
- theReader->AddVariable("dphilljetjet",    &dphilljetjet_);
+//  theReader->AddVariable("dphilljetjet",    &dphilljetjet_);
 
  theReader->BookMVA(methodName,weightsfile);
 
@@ -42,13 +46,13 @@ double GetWWewkMVA::getValue(
    double jetpt2,
    double detajj,
    double logmjj,
-   double pt1,
-   double pt2,
-   double ptll,
+//    double pt1,
+//    double pt2,
+//    double ptll,
    double zeppl1,
    double zeppl2,
-   double zeppll,
-   double dphilljetjet
+   double zeppll
+//    double dphilljetjet
                           ){
 
     if (!isInit) {
@@ -60,13 +64,13 @@ double GetWWewkMVA::getValue(
     jetpt2_ = jetpt2_;
     detajj = detajj_;
     logmjj = logmjj_;
-    pt1 = pt1_;
-    pt2 = pt2_;
-    ptll = ptll_;
+//     pt1 = pt1_;
+//     pt2 = pt2_;
+//     ptll = ptll_;
     zeppl1 = zeppl1_;
     zeppl2 = zeppl2_;
     zeppll = zeppll_;
-    dphilljetjet = dphilljetjet_; 
+//     dphilljetjet = dphilljetjet_; 
 
     return theReader->EvaluateMVA( methodname_ );
 
